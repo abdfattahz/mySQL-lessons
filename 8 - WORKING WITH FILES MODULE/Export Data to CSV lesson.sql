906ba66a0c799d2d07f60349d8bff9ffc7e01d36
abdfattahz <203197396+abdfattahz@users.noreply.github.com>
2024-08-12 08:20:00 +0800

    Export MySQL data to CSV

---
+++ 8 - WORKING WITH FILES MODULE/Export Data to CSV lesson.sql
@@ -0,0 +1,14 @@
+# Exporting Data to CSV
+
+
+SELECT * 
+FROM (
+ SELECT c.customer_id, 
+ first_name, 
+ order_total, 
+ ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_total DESC) as row_num 
+FROM customers c 
+JOIN customer_orders co 
+   ON c.customer_id = co.customer_id ) AS row_table 
+WHERE row_num < 3 
+;

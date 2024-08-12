fca1c6a7c53e43bf7244242666690c3ea30034b8
abdfattahz <203197396+abdfattahz@users.noreply.github.com>
2024-08-12 10:20:00 +0800

    Data cleaning: rename columns and remove duplicates

---
+++ 8 - WORKING WITH FILES MODULE/Changing Column Name lesson.sql
@@ -0,0 +1,8 @@
+SELECT * FROM bakery.product_suggestions;
+
+ALTER TABLE product_suggestions RENAME COLUMN `ï»¿product_suggestion_id` TO `product_suggestion_id`;
+# this query is to change the column name
+# use ` to change the column name
+# ` means that it's a column
+
+
